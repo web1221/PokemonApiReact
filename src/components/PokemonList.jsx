5ec5853af4c93de05b7e71b65b4efdb8c54ee79e
Student <example@example.com>
2020-02-13 16:45:09 -0800

    add pokemon cards

--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -3,11 +3,16 @@ import YourMom from './YourMom';
 import ORange from '../orange';
 
 export default function PokemonList() {
-    const myRange = new ORange(1, 50);
+    const myRange = new ORange(1, 800);
+    const gritty = {
+        display: 'grid',
+        gridTemplateColumns: '1fr 1fr 1fr 1fr 1fr',
+        gridGap: '5rem',
+    };
     return (
-        <div>
-            {myRange.map(e => {
-                return <YourMom pokeid={e} />;
+        <div style={gritty}>
+            {myRange.map((e, i) => {
+                return <YourMom pokeid={e} key={i} />;
             })}
         </div>
     );
--- src/components/YourMom.jsx
+++ src/components/YourMom.jsx
@@ -1,17 +1,58 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import pokeTypes from '../poketypes';
+
+const capitalize = s => {
+    return s.charAt(0).toUpperCase() + s.slice(1);
+};
 
 export default function YourMom(props) {
-    const [pokemon, setPokemon] = useState({});
+    const [pokemon, setPokemon] = useState(undefined);
     axios
         .get(`https://pokeapi.co/api/v2/pokemon/${props.pokeid}/`)
         .then(res => {
-            setPokemon(res.data.sprites.front_default);
-            console.log(res);
+            setPokemon(res.data);
+            console.log(res.data);
         });
+    const card = {
+        width: '14rem',
+        height: '20rem',
+        border: '3px solid',
+        borderRadius: '10px',
+        padding: '1rem',
+    };
+    const imgContainer = {
+        width: '90%',
+        border: '2px solid black',
+        margin: '1rem auto',
+        backgroundColor: 'white',
+    };
+    const imgStyle = {
+        height: '10rem',
+        display: 'block',
+        margin: '0 auto',
+    };
+    const colorSpread = arr => {
+        return {
+            ...card,
+            backgroundColor: pokeTypes[arr[arr.length - 1]['type']['name']],
+            boxShadow: `0 0 5px 5px ${pokeTypes[arr[0].type.name]}`,
+        };
+    };
     return (
-        <div>
-            <img src={pokemon} alt="yolo" />
+        <div style={pokemon ? colorSpread(pokemon.types) : card}>
+            <div style={imgContainer}>
+                <img
+                    src={pokemon ? pokemon.sprites.front_default : ''}
+                    style={imgStyle}
+                    alt="yolo"
+                />
+            </div>
+            <div>
+                <h2 style={{ textAlign: 'center' }}>
+                    {pokemon ? capitalize(pokemon.name) : 'Loading...'}
+                </h2>
+            </div>
         </div>
     );
 }
